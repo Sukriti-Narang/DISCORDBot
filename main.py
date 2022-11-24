0cd25a7bab2afd0f630ad42aa3bf34a49a3cc88d
Sukriti-Narang <100237223+Sukriti-Narang@users.noreply.github.com>
2022-11-25 05:26:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+# #MTA0NDIwNDkwMDgxODYzMjc4NQ.GAOpKZ.bvJkJKiJeU8tO_0tMYMmt1-rX5q60YWgOWjGqo
+# # bot.py
+
+
 
 import discord
 import os
@@ -6,96 +10,96 @@ import json
 import random
 from dotenv import load_dotenv
 from discord.ext import commands
-
-bot = commands.Bot(command_prefix="$",intents=discord.Intents.default())
-
-
-
-load_dotenv()
-
-client = discord.Client(intents=discord.Intents.default())
-
-def get_quote():
-
-  SUKRITI_quotes=["You can do it :)"," Ups in life come after downs","When the going gets tough, the tough gets going !"]
-  response = random.choice(SUKRITI_quotes)
-
-  return(quote)
+import discord
+from discord import Member
+from discord.ext import commands
+from discord.ext.commands import has_permissions, MissingPermissions
+#intents = discord.Intents(messages=True)
+#client = discord.Client(intents=intents)
 
 
-@client.event
-async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+
+# intents = discord.Intents.all()
+# client = discord.Client(intents=intents)
+
+# #load_dotenv()
+
+
+# client = discord.Client(intents=discord.Intents.all())
+client = commands.Bot(command_prefix='!')
+#client=discord.Client()
+
+# @client.event
+# async def on_ready():
+#     print(f'{client.user} has connected to Discord!')
+#     await client.process_commands(message="Go")
+# @client.event
+# async def on_message(ctx):
+#     await ctx.send("HEY THERE !")
+
+
 
 @client.event
 async def on_message(message):
-    if message.author == client.user:
-        return
+	username = str(message.author).split("#")[0]
+	channel = str(message.channel.name)
+	user_message = str(message.content)
 
-    msg = message.content
-#REPLY FOR HIII
-    if msg.startswith('$hiii'):
-        await message.channel.send('Heyy there welcome to sukriti bot :) ')
+	#print(f'Message {user_message} by {username} on {channel}')
 
-  #INCASE YOU NEED MOTIVATION , WE GIVE YOU ENCOURAGING MSGS 
-    if msg.startswith('$motivate'):
-      encourage_msg = response = random.choice(get_quotes())
-      await message.channel.send(encourage_msg)
+	if message.author == client.user:
+		return
 
-
-    bad_words = ["hell", "die", "suck", "loser"]
-
-    starter_encouragements = [
-  "Warning! Ban", "These actions can lead you to get banned"
-]
-
-#YOU MIGHT GET A BAN IF YOU USE BAD WORDS
-    if any(word in msg for word in bad_words):
-      await message.channel.send(random.choice(starter_encouragements))
-    
-
-#client.run('MTA0NDIwNDkwMDgxODYzMjc4NQ.G4KWRg.KAklR-BSEE_Sa__6npjzYbpsRdrHub-fkzus
+	if channel == "random":
+		if user_message.lower() == "hello":
+			await message.channel.send(f'Hello {username}, "Welcome to sukriti bot')
+			return
+		elif user_message.lower() == "bye":
+			await message.channel.send(f'Bye {username}')
+		elif user_message.lower() == "motivate":
+			jokes = ["You got this !", " When going get's tough, the tough gets going","Coffee and Productivity !"]
+			await message.channel.send(random.choice(jokes))
+		elif user_message.lower() == "die" or user_message.lower() == "suck":
+			await message.channel.send(f"{message.author.mention} Message contains  a banned word.")
+	await client.process_commands(message)
 
 
-#USE print COMMAND IN BOT TO PRINT ANYTHING
-@bot.command()
-async def print(ctx, *msgs):
-	reply = ""
+@client.command(
+	# ADDS THIS VALUE TO THE $HELP PRINT MESSAGE.
+	help="Looks like you need some help.",
+	# ADDS THIS VALUE TO THE $HELP MESSAGE.
+	brief="Prints the list of values back to the channel."
+)
+async def say(ctx, *abs):
+	ans = ""
+	ans=ans+" You say - "
+	# LOOPS THROUGH THE LIST OF ARGUMENTS THAT THE USER INPUTS.
+	for ab in abs:
+		ans = ans + " " + ab
 
-	for msg in msgs:
-		reply = reply + " " + msg
-
-	await ctx.channel.send(reply)
-
-
-
-#PLAY A DICE GAME
-@bot.command(name='lucky_number')
-async def roll(ctx, Date_Of_Birth: int):
-    luck = [
-        str(random.choice(range(Date_Of_Birth,Date_Of_Birth + 7)))
-        for _ in range(Date_Of_Birth)
-    ]
-    await ctx.send(', '.join(luck))
+	# SENDS A MESSAGE TO THE CHANNEL USING THE CONTEXT OBJECT.
+	await ctx.channel.send(ans)
 
 
+@client.command(name='colour')
+async def test(ctx): 
+    retStr = str("""```diff\nHow's green ????```""") 
+    await ctx.send(retStr)
+@client.command()
+@has_permissions(ban_members=True)
+async def ban(ctx, member:discord.Member,*,reason=None):
+    await member.ban(reason=reason)
+    await ctx.send(f'The User {member} has been banned from the server')
+@ban.error
+async def ban_error(ctx,error):
+    if isinstance(error,commands.MissingPermissions):
+        await ctx.send("You do not have required permission for the action performed")
 
-#CREATE A NEW CHANNEL IF YOU ARE ADMIN
-@bot.command(name='create-channel')
-@commands.has_role('admin')
-async def create_channel(ctx, channel_name='SukritiBot'):
-    guild = ctx.guild
-    existing_channel = discord.utils.get(guild.channels, name=channel_name)
-    if not existing_channel:
-        print(f'Creating a new channel: {channel_name}')
-        await guild.create_text_channel(channel_name)
 
-@bot.event
-async def on_command_error(ctx, error):
-    if isinstance(error, commands.errors.CheckFailure):
-        await ctx.send('You do not have the correct role for this command.')
-      
-my_secret = os.environ['TOKEN']
 
-#client.run('my_secret')
-#client.run(os.getenv('my_secret'))
+
+
+
+client.run('MTA0NDIwNDkwMDgxODYzMjc4NQ.GAOpKZ.bvJkJKiJeU8tO_0tMYMmt1-rX5q60YWgOWjGqo')
+
+
